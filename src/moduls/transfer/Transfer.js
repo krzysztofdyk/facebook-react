0fb4e17b8e455bf7bbcf22e5b4a26efb6f385173
kDyk <dyk.krzysztof@gmail.com>
2023-06-23 22:49:43 +0200

    Transfers: request added

--- src/moduls/transfer/Transfer.js
+++ src/moduls/transfer/Transfer.js
@@ -5,11 +5,10 @@ import QuestionDelete from "../../components/open/QuestionDelete";
 import CreateTransfer from "../../components/open/CreateTransfer";
 
 function Transfer() {
-  const [id, setId] = useState(0);
-
   const [openCreateTransferModal, setCreateTransferModal] = useState(false);
   const [openEditTarnsferModal, setEditTransferModal] = useState(false);
   const [openQuestionDeleteModal, setOpenQuestionDeleteModal] = useState(false);
+  const [responseData, setResponseDat] = useState([]);
 
   function handleCreateTransferModal() {
     setCreateTransferModal(true);
@@ -29,7 +28,20 @@ function Transfer() {
     setOpenQuestionDeleteModal(true);
   }
 
-  const data = [
+  const getTransfers = () => {
+    fetch(`http://localhost:8080/transfers`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("tokenCookie"),
+      },
+    }).then((data) => setResponseDat(data));
+    console.log("Start showing the transfers");
+    console.log({ responseData });
+    console.log("Stop showing the transfers");
+  };
+
+  const exampleResponse = [
     {
       id: "1",
       title: "pizza",
@@ -50,16 +62,6 @@ function Transfer() {
       date: "2022-10-15",
       time: "20:09",
     },
-    {
-      id: "3",
-      title: "zakupy",
-      amount: "100",
-      currency: "PLN",
-      sender: "Kris",
-      receiver: "Anna",
-      date: "2022-10-15",
-      time: "20:09",
-    },
   ];
 
   return (
@@ -72,37 +74,45 @@ function Transfer() {
           <button className="button-cancel" onClick={handleOpenQuestionDeleteModal}>
             Delete
           </button>
-          <button onClick={handleEditTransferModal}>Edit </button>
+          <button onClick={getTransfers}>Get Transfers </button>
           <button onClick={handleCreateTransferModal}>Create transfer </button>
         </div>
         <div className="body-content">
           <div className="account-inputs">
-            <div className="mytable">
+            <div className="table">
               <table>
-                <tr>
-                  <th>Id</th>
-                  <th>Title</th>
-                  <th>Amount</th>
-                  <th>Currency</th>
-                  <th>Sender</th>
-                  <th>Receiver</th>
-                  <th>Date</th>
-                  <th>Time</th>
-                </tr>
-                {data.map((val, key) => {
-                  return (
-                    <tr key={key}>
-                      <td>{val.id}</td>
-                      <td>{val.title}</td>
-                      <td>{val.amount}</td>
-                      <td>{val.currency}</td>
-                      <td>{val.sender}</td>
-                      <td>{val.receiver}</td>
-                      <td>{val.date}</td>
-                      <td>{val.time}</td>
-                    </tr>
-                  );
-                })}
+                <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                    <th>Sender</th>
+                    <th>Receiver</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                  </tr>
+                </thead>
+                {() => {
+                  responseData.forEach((transferObject) => {
+                    transferObject.map((val, key) => {
+                      return (
+                        <tbody>
+                          <tr key={key}>
+                            <td>{val.id}</td>
+                            <td>{val.title}</td>
+                            <td>{val.amount}</td>
+                            <td>{val.currency}</td>
+                            <td>{val.sender}</td>
+                            <td>{val.receiver}</td>
+                            <td>{val.date}</td>
+                            <td>{val.time}</td>
+                          </tr>
+                        </tbody>
+                      );
+                    });
+                  });
+                }}
               </table>
             </div>
           </div>
